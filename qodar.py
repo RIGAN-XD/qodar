660060a93ecacebf2a208fb5d86ce4466acb9eeb
My rigan <164660678+RIGAN-XD@users.noreply.github.com>
2024-04-12 23:19:18 +0700

    Create qodar.py

---
+++ qodar.py
@@ -0,0 +1,16 @@
+import os, sys, time
+from time import sleep as waktu
+
+h = '\x1b[1;92m'  # Hijau
+xxx = '\33[m'
+
+def animasi(berjalan):
+    for gas in berjalan + "\n":
+        sys.stdout.write(gas)
+        sys.stdout.flush()
+        time.sleep(0.10)
+        
+if __name__=="__main__":
+        os.system('git pull')
+        animasi(f'\n{xxx}   ({h}ANJING SEMUAk{xxx})')
+        waktu(3)
